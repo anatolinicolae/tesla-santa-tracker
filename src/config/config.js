7346e074f243543fcbecbcd1a7fb488f35becdec
Anatoli Nicolae <an@thundersquared.com>
2024-12-22 02:38:48 +0100

    Fix deploy
    Install all deps
    
    Fix workflows
    
    Clean destination
    
    Fix env var
    
    Fix workflow
    
    Use proper env vars

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 const config = {
     zoomLevel: 12,
     supabase_source_table: 'santa_positions',
-    clarity_project_id: import.meta.env.CLARITY_PROJECT_ID,
+    clarity_project_id: import.meta.env.VITE_CLARITY_PROJECT_ID,
 };
 
 export default config;
